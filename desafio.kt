20270e2ac0b63c3c74360c829ab05aa4efabf9c7
Jean Maronez <jeanmaronez@gmail.com>
2024-05-15 13:53:35 -0400

    Primeiras implementações.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,36 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel {
+                 BASICO,
+    INTERMEDIARIO,
+    AVANCADO
+}
 
-class Usuario
+data class Usuario(
+        val nome: String,
+        val Idade: Int
+)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(
+        val nome: String,
+        val duracao: Int,
+        val nivel: List<Nivel>
+)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+    data class Formacao(
+        val nome: String,
+        val conteudos: List<ConteudoEducacional>
+) {
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
+
+    fun matricular(vararg usuario: Usuario) {
         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+fun main(vararg) {
+    // TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario = Usuario("Jean", 31),
+    val conteudo = ConteudoEducacional("Aprendendo Funções em Kotlin", 02, Nivel.INTERMEDIARIO),
+    val formacao = Formacao("Formação em Kotlin", Nivel.AVANCADO)
 }
